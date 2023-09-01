d99e3552fa61b832e779b0b7d37f3e7fd4439396
甄健豪 <@.>
2023-08-31 21:28:55 -0400

    opacity = 0.6

--- config.h
+++ config.h
@@ -111,7 +111,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 2;
 
 /* bg opacity */
-float alpha = 0.4;
+float alpha = 0.6;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
